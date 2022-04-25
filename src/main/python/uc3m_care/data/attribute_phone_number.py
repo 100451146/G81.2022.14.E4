79ae56a71b5f3ba33da86b6c0b1a821f94befd67
Ángel del Pozo <adelpozoman@gmail.com>
2022-04-25 22:03:16 +0200

    add old phone code

--- src/main/python/uc3m_care/data/attribute_phone_number.py
+++ src/main/python/uc3m_care/data/attribute_phone_number.py
@@ -6,3 +6,12 @@ class PhoneNumber(Attribute):
         self._validation_pattern = r"^(\+)[0-9]{11}"
         self._error_message = "phone number is not valid"
         self._attr_value = self._validate(attr_value)
+
+
+        #try:
+        #    myregex = re.compile(r"^(\+)[0-9]{11}")
+        #    res = myregex.fullmatch(data["ContactPhoneNumber"])
+        #    if not res:
+        #        raise VaccineManagementException("phone number is not valid")
+        #except KeyError as ex:
+        #    raise VaccineManagementException("Bad label contact phone") from ex
