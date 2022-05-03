322e2a2b326546283780806e2bf985c7681d6050
Francisco Gallardo <100451146@alumnos.uc3m.es>
2022-05-03 22:19:43 +0200

    Vaccine Log

--- src/main/python/uc3m_care/storage_mangement/vaccinated_storage.py
+++ src/main/python/uc3m_care/storage_mangement/vaccinated_storage.py
@@ -2,6 +2,7 @@ import datetime
 
 from uc3m_care.cfg.vaccine_manager_config import JSON_FILES_PATH, VACCINES_STORE
 from uc3m_care.storage_mangement.json_storage import JsonStore
+from uc3m_care.vaccine_log import VaccineLog
 
 
 class VaccinationStorage(JsonStore):
@@ -13,7 +14,8 @@ class VaccinationStorage(JsonStore):
             vaccinated_list = JsonStore.load_json(VaccinationStorage._FILE_PATH)
         except FileNotFoundError:
             vaccinated_list = []
+        my_vaccine_log = VaccineLog(date_signature)
         # append the date
-        vaccinated_list.extend({date_signature.__str__(), datetime.datetime.utcnow().__str__()})
+        vaccinated_list.append(my_vaccine_log.__dict__)
 
         JsonStore.save_json_data(vaccinated_list, VACCINES_STORE)
---
+++ src/main/python/uc3m_care/vaccine_log.py
@@ -0,0 +1,20 @@
+import re
+from datetime import datetime
+from uc3m_care.exception.vaccine_management_exception import VaccineManagementException
+
+class VaccineLog():
+    def __init__(self, date_signature):
+        self.__date_signature = self.validate_date_signature(date_signature)
+        self.__date = datetime.utcnow().__str__()
+
+    def validate_date_signature(self, date_signature: str)-> None:
+
+        date_signature_pattern = re.compile(r"[0-9A-fA-F]{64}$")
+        result = date_signature_pattern.fullmatch(date_signature)
+        if not result:
+            raise VaccineManagementException("date_signature format is not valid")
+        return date_signature
+
+    @property
+    def date_signature(self):
+        return self.__date_signature
