9657662b26039b39f935fea0382d3789be8c6611
Ángel del Pozo <adelpozoman@gmail.com>
2022-05-03 23:05:57 +0200

    small vaccine_log fix

--- src/main/python/uc3m_care/vaccine_log.py
+++ src/main/python/uc3m_care/vaccine_log.py
@@ -2,13 +2,14 @@ import re
 from datetime import datetime
 from uc3m_care.exception.vaccine_management_exception import VaccineManagementException
 
+
 class VaccineLog():
     def __init__(self, date_signature):
         self.validate_date_signature(date_signature)
         self.__date_signature = datetime.utcnow().__str__()
 
     @staticmethod
-    def validate_date_signature(date_signature: str)-> None:
+    def validate_date_signature(date_signature: str) -> str:
         date_signature_pattern = re.compile(r"[0-9A-fA-F]{64}$")
         result = date_signature_pattern.fullmatch(date_signature)
         if not result:
